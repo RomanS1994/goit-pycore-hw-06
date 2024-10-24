7cca47661603177717e4e6d0bee7730deae8305d
Roman Stryzhka <rstrizhko1994@gmail.com>
2024-10-24 09:35:24 +0200

    completed home_task

--- main.py
+++ main.py
@@ -8,26 +8,73 @@ class Field:
         return str(self.value)
 
 class Name(Field):
-    # реалізація класу
-		pass
+    pass
 
 class Phone(Field):
-    # реалізація класу
-		pass
+    def check_phone(self):
+        try:
+            if len(self.value) == 10 and self.value.isdigit():
+                return self.value 
+        except ValueError:
+            print("Phone number must be exactly 10 numerals")
 
 class Record:
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
 
-    # реалізація класу
+    def add_phone(self, phone_number):
+        phone = Phone(phone_number)
+        self.phones.append(phone)
+    
+    def remove_phone(self, phone_number):
+        try: 
+            self.phones.remove(phone_number)
+        except ValueError as e:
+            print(e)
+
+
+    def edit_phone(self, old_phone, new_phone):
+        for index, phone in enumerate(self.phones):
+            if phone == old_phone:
+                self.phones[index] = Phone(new_phone)
+                print(f"Phone {old_phone} changed to {new_phone}")
+                return
+            else:
+                print(f"Phone {old_phone} not found")
+    
+    def find_phone(self, phone_number):
+        try:
+            for phone in self.phones:
+                if phone.value == phone_number:
+                    return phone.value
+        except ValueError as e:
+            print(e)
+
+        
+
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
 class AddressBook(UserDict):
-    # реалізація класу
-		pass
+    def add_record(self, record):
+        self.data[record.name.value] = record
+        print(f'Contact {record.name} added' )
+        
+    def find(self, name):
+        if name in self.data:
+            return self.data[name]
+        else:
+            print(f'Contact with name "{name}" not found')
+            return None
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+        else:
+            print(f'Contact with name "{name}" not found')
+
 
 
 
